96289b2ac34b64b475d20c447da3bf07fcc534ab
Daniel Tsirlin <daniel.tsirlin@rea-group.com>
2021-07-08 19:29:59 +1000

    Read files in directory

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import webbrowser as wb
 import pathlib as pl
+import os
+import secrets
 
-chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
-
-def openBrowser(url):
+def openURL(url):
     wb.get('safari').open_new(url)
     return 
 
-def openURL(url):
-    return
+def getFiles(parentDirectory):
+    print('Reading files from:',parentDirectory)
+    return os.listdir(parentDirectory)
 
+def sortedFiles(parentDirectory):
+    return sorted(getFiles(parentDirectory))
 
-
-if __name__ == "__main__":
-    print("sup dog")
-    openBrowser("file://" + str(pl.Path().resolve()) + "/htmls/1.html")
-    # os.chdir(r"htmls")
-    #openBrowser("http://www.google.com")
+if __name__ == '__main__':
+    print('starting')
+    files = sortedFiles(str(pl.Path().resolve()) + '/htmls')
+    print(files)
+    # print(secrets.SystemRandom().choice(files))
+    print('finishing')
+    # openURL('file://' + str(pl.Path().resolve()) + '/htmls/1.html')
